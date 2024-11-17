ae7829acd56d40d59e292e44be1d306f8e48c587
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-17 16:46:41 +0100

    Add IngredientsCard into IngredientsPage and refactor

--- ingredish/src/pages/IngredientsPage.tsx
+++ ingredish/src/pages/IngredientsPage.tsx
@@ -1,20 +1,11 @@
-import { bevarages, fruits, vegetables, proteins, dairy, grains_and_starches, spices_and_herbs, oils_and_fats, sweeteners } from '../types/Ingredients.jsx';
-
+import { IngredientsCard } from "../components/IngredientsCard.js";
+import * as ingredients from "../types/Ingredients.jsx";
 
 export default function IngredientsPage() {
-  
-
   return (
     <div>
-      {bevarages}
-      {fruits}
-      {vegetables}
-      {proteins}
-      {dairy}
-      {grains_and_starches}
-      {spices_and_herbs}
-      {oils_and_fats}
-      {sweeteners}
+      <IngredientsCard {...ingredients}
+      />
     </div>
   );
 }
