3f6ed74225e3227d5121b2754e01c3b6cead22aa
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-18 17:14:55 +0100

    Add logo to the header

--- ingredish/src/components/WelcomePageHeader.tsx
+++ ingredish/src/components/WelcomePageHeader.tsx
@@ -1,11 +1,11 @@
 import { SignedOut, SignInButton } from "@clerk/clerk-react";
-// import logo from "../assets/logo.png";
+import logo from "../assets/images/logo.png";
 
 export default function WelcomePageHeader() {
   return (
     <header className="welcome-page-header-container">
       <SignedOut>
-        {/* <img src={logo} alt="logo" className="logo" /> */}
+        <img src={logo} alt="logo" className="logo" />
         <SignInButton />
       </SignedOut>
     </header>
