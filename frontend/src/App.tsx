20e21fe052e9c750f51d720b503e1cce559f86f1
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-19 12:47:20 +0100

    Add route for ingredients page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,18 @@ import IngredientsPage from "./pages/IngredientsPage";
 import "./style/styles.css";
 import WelcomePageHeader from "./components/WelcomePageHeader";
 import WelcomePage from "./pages/WelcomePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export function App() {
   return (
-    <>
+    <BrowserRouter>
       <SignedOut>
         <WelcomePageHeader />
         <WelcomePage />
       </SignedOut>
-      {/* <h1>Ingredish</h1>
-      <IngredientsPage /> */}
-    </>
+      <Routes>
+        <Route path="/" element={<IngredientsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
