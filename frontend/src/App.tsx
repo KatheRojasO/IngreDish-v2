52cff7ea44fa5ca1e2b99fc7a2bd72069adccc7f
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-19 19:38:10 +0100

    Add routes for favorite page and instructions page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { SignedOut } from "@clerk/clerk-react";
 import IngredientsPage from "./pages/IngredientsPage";
-import "./style/styles.css";
 import WelcomePageHeader from "./components/WelcomePageHeader";
 import WelcomePage from "./pages/WelcomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecipesPage } from "./pages/RecipesPage";
-import { useState } from "react";
+import RecipeInstructionsPage from "./pages/RecipeInstructionsPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import "./style/styles.css";
 
 
 export function App() {
@@ -22,6 +24,8 @@ export function App() {
       <Routes>
         <Route path="/" element={<IngredientsPage selectedIngredients={selectedIngredients} setSelectedIngredients={setSelectedIngredients}/>} />
         <Route path="/recipes" element={<RecipesPage selectedIngredients={selectedIngredients} />} />
+        <Route path="/recipe/:recipeId" element={<RecipeInstructionsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </BrowserRouter>
   );
