7bcde6cf60fcd46b55505ea8ecdf3abdcc6ad832
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-20 16:15:13 +0100

    Add message when there are no favorite recipes

--- frontend/src/pages/FavoritesPage.tsx
+++ frontend/src/pages/FavoritesPage.tsx
@@ -4,10 +4,11 @@ import { Recipes } from "../components/Recipes";
 import { useEffect, useState } from "react";
 import { Recipe } from "../types/Recipe";
 import { fetchFavoritesByUserId } from "../helper/UserFavoritesHelper";
+import { Link } from "react-router-dom";
 
 export default function FavoritesPage() {
   const { user } = useUser();
-  const [favorites, setFavorites] = useState<Recipe[]>();
+  const [favorites, setFavorites] = useState<Recipe[]>([]);
 
   useEffect(() => {
     if (!user) return;
@@ -18,12 +19,25 @@ export default function FavoritesPage() {
     }
 
     fetchFavorites();
-  }, [ user]);
+  }, [user]);
 
   return (
     <div>
       <Header />
-      <Recipes recipesInfo={favorites || []} isFavoritePage={true}/>
+      {favorites === null ? (
+        <div className="favorites-alert">
+          <p>Loading your favorites...</p>
+        </div>
+      ) : favorites.length === 0 ? (
+        <div className="favorites-alert">
+          <p>You don't have any favorites yet. ¡Explore recipes and save your favorites to see them here! ❤️</p>
+          <Link to="/">
+            <button className="go-back-button">Go back to ingredients page</button>
+          </Link>
+        </div>
+      ) : (
+        <Recipes recipesInfo={favorites} isFavoritePage={true} />
+      )}
     </div>
   );
 }
