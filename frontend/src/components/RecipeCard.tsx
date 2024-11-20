e2bdb2c376d69f9c69a727332b23adc6f0c9fc0f
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-20 15:04:31 +0100

    Minor style fixes

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -6,10 +6,7 @@ import { useUser } from "@clerk/clerk-react";
 import { addFavorite, removeFavorite } from "../helper/UserFavoritesHelper";
 import { Link } from "react-router-dom";
 
-export function RecipeCard({
-  recipeInfo,
-  defautlFavoriteValue,
-}: RecipeCardProps) {
+export function RecipeCard({ recipeInfo, defautlFavoriteValue }: RecipeCardProps) {
   const [liked, setLiked] = useState(defautlFavoriteValue);
   const { user } = useUser();
 
@@ -29,16 +26,12 @@ export function RecipeCard({
   return (
     <div className="card">
       <h3>{recipeInfo.title}</h3>
-      <img
-        src={recipeInfo.image}
-        alt={recipeInfo.title}
-        className="recipe-image"
-      />
+      <img src={recipeInfo.image} alt={recipeInfo.title} className="recipe-image" />
       <div className="card-buttons">
         <button onClick={toggleLike} className="like-button">
           {liked ? <HeartFilledIcon /> : <HeartOutlineIcon />}
         </button>
-        <Link to={{ pathname: `/recipe/${recipeInfo.id}`}}>
+        <Link to={{ pathname: `/recipe/${recipeInfo.id}` }}>
           <button className="recipe-button">Go to recipe</button>
         </Link>
       </div>
