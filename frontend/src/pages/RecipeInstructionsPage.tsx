cbfa0096950136a73a7418283f6b15e813b8f306
KatheRojasO <kathe.rojaso@gmail.com>
2024-11-21 14:25:24 +0100

    Add container for comments, and message for when there's no notes

--- frontend/src/pages/RecipeInstructionsPage.tsx
+++ frontend/src/pages/RecipeInstructionsPage.tsx
@@ -5,6 +5,7 @@ import { fetchRecipeInstructions } from "../helper/SpoonacularApiHelper";
 import { Link, useParams } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
+import Trash from "../assets/icons/Trash";
 
 export default function RecipeInstructionsPage() {
   const { recipeId } = useParams<{ recipeId: string }>();
@@ -43,31 +44,42 @@ export default function RecipeInstructionsPage() {
           <button className="go-back-button">Go back to recipes page</button>
         </Link>
       </div>
-      <div className="instructions-card">
-        <h3>{recipeInfo?.title}</h3>
-        <img src={image} alt={recipeInfo?.title} className="instructions-image" />
-        <ol>{stepElements}</ol>
-      </div>
-      <div className="note-container">
-        <h3>Your Notes</h3>
-        <textarea
-          value={note}
-          onChange={handleNoteChange}
-          placeholder="Write what you liked or didn't like about this recipe..."
-          className="note-textarea"
-        />
-        <button className="save-note-button" onClick={handleNoteSave}>
-          Save Note
-        </button>
-        <div className="saved-notes">
-          {savedNote.length > 0 && <h4>Your notes about this recipe</h4>}
-          <ul>
-            {savedNote.map((savedNote, index) => (
-              <li key={index} className="saved-note-item">
-                ⭐ {savedNote}
-              </li>
-            ))}
-          </ul>
+      <div className="content-container">
+        <div className="instructions-card">
+          <h3>{recipeInfo?.title}</h3>
+          <img src={image} alt={recipeInfo?.title} className="instructions-image" />
+          <ol>{stepElements}</ol>
+        </div>
+        <div className="note-container">
+          <div className="saved-notes">
+            {savedNote.length > 0 ? (
+              <>
+                <h4>Your personal notes about this recipe</h4>
+                <ul>
+                  {savedNote.map((savedNote, index) => (
+                    <div className="note-list-container">
+                      <div><Trash /></div>
+                      <li key={index} className="saved-note-item">
+                        {savedNote}
+                      </li>
+                    </div>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <p className="no-notes-message">No notes yet. Start by adding your first note!</p>
+            )}
+          </div>
+          <h3>Your Notes</h3>
+          <textarea
+            value={note}
+            onChange={handleNoteChange}
+            placeholder="Write what you liked or didn't like about this recipe..."
+            className="note-textarea"
+          />
+          <button className="save-note-button" onClick={handleNoteSave}>
+            Save Note
+          </button>
         </div>
       </div>
     </div>
